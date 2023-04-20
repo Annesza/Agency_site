9f59e42edd352840e4a9e6a175ce0a4eaf217c06
Anna <annasszafranska@gmail.com>
2023-04-20 15:58:52 +0200

    Added square expansion and additional breakpoints

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const hamburgerBar = document.querySelectorAll('.hamburger__bar')
 const nav = document.querySelector('.nav')
 const list = document.querySelector('.list')
 
-hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger--open')
     nav.classList.toggle('nav--open')
     list.classList.toggle('list--open')
+})
+
+const square = document.querySelector('.square__description')
+const icon = document.querySelector('.square__icon')
+const info = document.querySelector('.square__info')
+const wrapper = document.querySelector('.square__wrapper')
+
+
+icon.addEventListener('click', () => {
+    icon.classList.toggle('square__icon--open')
+    square.classList.toggle('square__description--open')
+    wrapper.classList.toggle('square__wrapper--open')
+    info.classList.toggle('square__info--open')
 })
