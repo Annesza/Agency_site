cb64c21dca6439f3f90c36704fd86da6c8399393
Jakub Jóźwiak <jozwiak.ja@icloud.com>
2023-04-20 16:22:22 +0200

    Commit last changes

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const hamburger = document.querySelector('.hamburger')
 const hamburgerBar = document.querySelectorAll('.hamburger__bar')
 const nav = document.querySelector('.nav')
 const list = document.querySelector('.list')
+const expendIcon = document.querySelector('.square__icon')
+
+const squareDescription = document.querySelector('.square__description')
 
 hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle('hamburger--open')
     nav.classList.toggle('nav--open')
     list.classList.toggle('list--open')
+})
+
+expendIcon.addEventListener('click', ()=>{
+    squareDescription.classList.toggle('square__description--open')
 })
